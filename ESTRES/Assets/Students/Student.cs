ddf2f789aa8d6714435bd18b6abb74f98e7d2c19
Jose Luis <joseluisrojasaranda@gmail.com>
2019-10-17 19:45:39 -0500

    Detalles de juego, fin primer dia

--- ESTRES/Assets/Students/Student.cs
+++ ESTRES/Assets/Students/Student.cs
@@ -17,6 +17,8 @@ public class Student : MonoBehaviour
     public int level;
     public int startLevel;
 
+    public int score;
+
 
     // Start is called before the first frame update
     void Start()
